ec674d00c32ab1063780adbfb0bc609e2b6a1d73
wesley sharpe <wesmon50@yahoo.com>
2020-09-13 12:34:51 -0400

    pickup starting

--- BlankKarel.java
+++ BlankKarel.java
@@ -9,6 +9,81 @@ import stanford.karel.*;
 public class BlankKarel extends SuperKarel {
 	public void run() {
 		/* You fill this in */
+		int rowCount= 1;
+		while(frontIsClear()) {
+			if(rowCount % 2 ==0) {
+				CheckBallet();
+			}
+			
+			rowCount++;
+			while(notFacingEast()) {
+				turnLeft();
+			}
+			if(frontIsClear()) {
+				move();
+			}
+
+		}
+
+	}
+
+	private void CheckBallet() {
+		// TODO Auto-generated method stub
+
+		topOfBallet();
+		System.out.println("top of ballot finishes");
+		goDownBallet();
+	}
+
+	private void goDownBallet() {
+		// TODO Auto-generated method stub
+		int i =0;
+		while(notFacingSouth()) {
+			turnLeft();
+		}
+		
+		
+		while(frontIsClear()) {
+			if(beepersPresent()) {
+				i++;
+			}
+			move();
+		}
+		if(i == 2 || i==0) {
+			while(notFacingNorth()) {
+				turnLeft();
+			}
+			move();
+		}else {
+			System.out.println("pickup starts");
+			startPickUp();
+			System.out.println("pickup ends");
+		}
+
+	}
+
+	private void startPickUp() {
+		// TODO Auto-generated method stub
+		while(notFacingNorth()) {
+			turnLeft();
+		}
+		while(frontIsClear()) {
+			while(beepersPresent()) {
+				pickBeeper();
+			}
+			move();
+		}
+	}
+
+	private void topOfBallet() {
+		// TODO Auto-generated method stub
+		while(notFacingNorth()) {
+			turnLeft();
+		}
+		while(frontIsClear()) {
+			move();
+			
+		}
 	}
 }
 
